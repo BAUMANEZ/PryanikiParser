cfff1db00723b3a8fddaec85f53a0adefda76f8f
Arseny <arseny.tokarev@gmail.com>
2021-03-04 13:36:39 +0300

    Initialized project

---
+++ PryanikiParser/PryanikiParser/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PryanikiParser
+//
+//  Created by Арсений Токарев on 04.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
